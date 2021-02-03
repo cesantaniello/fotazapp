ef92c31f3b9476f82675c479e70fa5cee4d88319
Carlos <carlos.e.santaniello@gmail.com>
2021-02-03 13:42:02 +0100

    2. Objeto Request

--- fotazapp/urls.py
+++ fotazapp/urls.py
@@ -1,12 +1,10 @@
 """Fotazapp URLs module"""
 from django.urls import path
-from django.http import HttpResponse
 
+from fotazapp import views
 
-def hello_world(request):
-    """Return a greeting"""
-    return HttpResponse('Hello, world!')
 
 urlpatterns = [
-    path('hello-world/', hello_world)
+    path('hello-world/', views.hello_world),
+    path('hi/', views.hi),
 ]
---
+++ fotazapp/views.py
@@ -0,0 +1,18 @@
+"""Fotazapp views"""
+
+# Django
+from django.http import HttpResponse
+
+# Utilities
+from datetime import datetime
+
+def hello_world(request):
+    """Return a greeting"""
+    return HttpResponse('Oh, hi! Current server time is {now}'.format(
+       now = datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
+    ))
+
+def hi(request):
+    """Hi."""
+    numbers = request.GET['numbers']
+    return HttpResponse('Hi!')
