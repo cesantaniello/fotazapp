609be547a9f45fd50caf6400f932694828f82158
Carlos <carlos.e.santaniello@gmail.com>
2021-02-03 22:29:19 +0100

    8. Modelo de usuario

--- fotazapp/urls.py
+++ fotazapp/urls.py
@@ -1,14 +1,1 @@
-"""Fotazapp URLs module"""
-from django.urls import path
 
-from fotazapp import views as local_views
-from posts import views as posts_views
-
-
-urlpatterns = [
-    path('hello-world/', local_views.hello_world),
-    path('sorted/', local_views.sorted_integers),
-    path('hi/<str:name>/<int:age>', local_views.say_hi),
-
-    path('posts/', posts_views.list_posts),
-]
