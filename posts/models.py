c7d977187f504ab04028074653b7de1c42c67047
Carlos <carlos.e.santaniello@gmail.com>
2021-02-03 20:25:23 +0100

    7. ORM de Django

--- posts/models.py
+++ posts/models.py
@@ -12,9 +12,15 @@ class User(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    is_admin = models.BooleanField(default=False)
+
     bio = models.TextField(blank=True)
 
     birthdate = models.DateField(blank=True, null=True)
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return email."""
+        return self.email
