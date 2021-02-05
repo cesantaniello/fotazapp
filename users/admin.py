2b7816eceaf5c9f02b89d712dfa9be563441719a
Carlos <carlos.e.santaniello@gmail.com>
2021-02-05 17:52:39 +0100

    11. Modificado dashboard de administracion

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,11 @@
 """User admin classes."""
 
 # Django
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
 # Models
+from django.contrib.auth.models import User
 from users.models import Profile
 
 @admin.register(Profile)
@@ -16,3 +18,39 @@ class ProfileAdmin(admin.ModelAdmin):
     search_fields = ('user__email', 'user__first_name', 'user__last_name', 'phone_number')
 
     list_filter = ('user__is_active', 'user__is_staff', 'created', 'modified')
+
+    fieldsets = (
+        ('Profile', {
+            'fields': (
+                ('user', 'picture'),),
+            }),
+        ('Extra info', {
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography')
+                ),
+            }),
+        ('Metadata', {
+            'fields': (
+                ('created', 'modified'),
+                ),
+            }),            
+    )
+
+    readonly_fields = ('created', 'modified',)
+
+class ProfileInline(admin.StackedInline):
+    """Profile in-line admin for users."""
+
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin."""
+
+    inlines = (ProfileInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
