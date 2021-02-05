cf5a4422cf2d308949c7de0dfef1b6a47e8ab140
Carlos <carlos.e.santaniello@gmail.com>
2021-02-05 20:29:16 +0100

    12. Creacion de modelo de posts

--- fotazapp/urls.py
+++ fotazapp/urls.py
@@ -2,6 +2,8 @@
 
 # Django
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from fotazapp import views as local_views
@@ -17,4 +19,5 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>', local_views.say_hi),
 
     path('posts/', posts_views.list_posts),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
