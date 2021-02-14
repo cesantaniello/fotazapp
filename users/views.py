8a329e7f7e700c4b43472c0da4aca214d7ec1bb3
Carlos <carlos.e.santaniello@gmail.com>
2021-02-14 23:03:19 +0100

    21. Model forms

--- users/views.py
+++ users/views.py
@@ -16,6 +16,7 @@ from users.models import Profile
 from users.forms import ProfileForm
 
 
+@login_required
 def update_profile(request):
     """Update a user's profile view."""
     profile = request.user.profile
