0fdffab7af1d1c6af0f26544f9c720af5ba87d16
Carlos <carlos.e.santaniello@gmail.com>
2021-03-08 16:32:06 +0100

    26. Generic auth views

--- users/views.py
+++ users/views.py
@@ -1,10 +1,9 @@
 """Users views."""
 
 # Django
-from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
+from django.contrib.auth import views as auth_views
 from django.urls import reverse, reverse_lazy
 from django.views.generic import DetailView, FormView, UpdateView
 
@@ -61,22 +60,12 @@ class UpdateProfileView(LoginRequiredMixin, UpdateView):
         username = self.object.user.username
         return reverse('users:detail', kwargs={'username': username})
 
-def login_view(request):
-    """Login view."""
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user:
-            login(request, user)
-            return redirect('posts:feed')
-        else:
-            return render(request, 'users/login.html', {'error': 'Invalid username and password'})
+class LoginView(auth_views.LoginView):
+    """Login view.""" 
+    template_name = 'users/login.html'
+    redirect_authenticated_user = True
 
-    return render(request, 'users/login.html')
+class LogoutView(auth_views.LogoutView):
+    """Logout view."""
 
-@login_required
-def logout_view(request):
-    """Logout a user"""
-    logout(request)
-    return redirect('users:login')
+    pass
